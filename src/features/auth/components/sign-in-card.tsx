713470b60ede42f36d44ef3e02f7ec5939d81d4a
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-03 18:43:10 +0530

    login card

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,7 +1,42 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const SignInCard = () => {
-  return <div>SignInCard </div>;
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader>
+        <CardTitle>Login to continue</CardTitle>
+      </CardHeader>
+      <CardDescription>
+        Use your email or another service to continue
+      </CardDescription>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value=""
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value=""
+            placeholder="Password"
+            type="Password"
+            required
+          />
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SignInCard;
