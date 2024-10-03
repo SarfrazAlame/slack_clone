87e2426f485f7394b29a34330c9bfcb570a5a8f7
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-03 21:45:24 +0530

    schema

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,24 @@
 "use client";
-
-import React from "react";
-import { api } from "../../../convex/_generated/api";
-import { useQuery } from "convex/react";
+import React from 'react'
 
 const page = () => {
-  const tasks = useQuery(api.tasks.get);
+  return (
+    <div>page</div>
+  )
+}
 
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-</main>
-};
+export default page
 
-export default page;
+// import React from "react";
+// import { api } from "../../../convex/_generated/api";
+// import { useQuery } from "convex/react";
+
+// const page = () => {
+//   const tasks = useQuery(api.tasks.get);
+
+//   return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//   {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+// </main>
+// };
+
+// export default page;
