2c9b2cee3e37877c8eced56e9084c231314f6c40
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-07 21:30:04 +0530

    editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import React from "react";
 import {Header} from "./Header";
+import ChatInput from "./ChatInput";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
@@ -34,6 +35,8 @@ const ChannelIdPage = () => {
 
   return <div className="flex flex-col h-full">
     <Header title={channel.name} />
+    <div className="flex-1"/>
+    <ChatInput/>
   </div>;
 };
 
