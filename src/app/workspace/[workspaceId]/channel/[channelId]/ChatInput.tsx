9d415b7f2df0c2398181313f6f132ba878e54d1b
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-08 06:19:27 +0530

    editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/ChatInput.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/ChatInput.tsx
@@ -1,12 +1,14 @@
-import Editor from '@/components/Editor'
-import React from 'react'
+import dynamic from "next/dynamic";
+import React from "react";
+
+const Editor = dynamic(() => import("@/components/Editor"), { ssr: false });
 
 const ChatInput = () => {
   return (
-    <div className='px-5 w-full'>
-        <Editor/>
+    <div className="px-5 w-full">
+      <Editor />
     </div>
-  )
-}
+  );
+};
 
-export default ChatInput
+export default ChatInput;
