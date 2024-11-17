db5042c320ff41164969336f4e41235ee927353c
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-11-17 17:40:04 +0530

    conversation ammong member

---
+++ convex/conversations.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { auth } from "./auth";
+
+export const createOrGet = mutation({
+    args: {
+        memberId: v.id("members"),
+        workspaceId: v.id("workspaces"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx)
+
+        if (!userId) {
+            throw new Error("Unauthorized")
+        }
+
+        const currentMember = await ctx.db.query("members").withIndex("by_worksapce_id_user_id", (q) => q.eq("workspaceId", args.workspaceId).eq("userId", userId)).unique()
+
+        const otherMember = await ctx.db.get(args.memberId)
+
+        if(!currentMember || !otherMember){
+            throw new Error("Member not found")
+        }
+    }
+})
